9efa282a996d119f7960a986aa8c226879701bc5
DrewPres <dretpres@ut.utm.edu>
2018-04-12 17:48:47 -0500

    added main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <BinarySearchTree.h>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    BinarySearchTree tree;
+
+    tree.insert("The",6);
+
+    tree.print();
+
+
+    return 0;
+}
